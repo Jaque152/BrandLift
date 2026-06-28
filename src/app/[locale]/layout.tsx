b61dbeec921240ba454cf93930e9f0998bdf20aa
JaknetSoft <jaknet.software.dev@gmail.com>
2026-06-27 20:55:01 -0600

    Analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import ClientBody from "./ClientBody";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Brand Lift | Creative Digital Agency",
@@ -17,11 +18,13 @@ interface RootLayoutProps {
 export default async function RootLayout({ children, params }: RootLayoutProps) {
   // 3. Resolvemos la promesa
   const { locale } = await params;
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html lang={locale || 'en'}>
       <body className="antialiased" suppressHydrationWarning>
         <ClientBody>{children}</ClientBody>
+        {gtmId && <GoogleTagManager gtmId={gtmId} />}
       </body>
     </html>
   );
